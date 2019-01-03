fe8974d545661d40af8840b3a9bafa75fd0fe5ee
dpaladin <dpaladinnyuu@gmail.com>
2019-01-03 17:31:59 -0600

    Update NumeroALetras.php
    Permite manejar números más grandes como string, y números con número de decimales diferente a 2

--- src/NumeroALetras.php
+++ src/NumeroALetras.php
@@ -60,71 +60,90 @@ class NumeroALetras
         'OCHOCIENTOS ',
         'NOVECIENTOS '
     ];
+    private static $OTROS = [
+        '', 'MILLON', 'BILLON', 'TRILLON', 'CUATRILLON', 'QUINTILLON', 'SEXTILLON', 'SEPTILLON', 'OCTILLON'
+    ];
 
     public static function convertir($number, $moneda = '', $centimos = '', $forzarCentimos = false)
     {
         $converted = '';
         $decimales = '';
-
-        if (($number < 0) || ($number > 999999999)) {
-            return 'No es posible convertir el numero a letras';
+        $number = (string) $number;
+        
+        $negativo = '';
+        if ($number[0] == '-') {
+            $negativo = 'MENOS ';
+            $number = substr($number, 1);
         }
 
-        $div_decimales = explode('.',$number);
+        $number = trim($number, '0');
+        $div_decimales = explode('.', $number);
+        $number = $div_decimales[0];
 
-        if(count($div_decimales) > 1){
-            $number = $div_decimales[0];
-            $decNumberStr = (string) $div_decimales[1];
-            if(strlen($decNumberStr) == 2){
-                $decNumberStrFill = str_pad($decNumberStr, 9, '0', STR_PAD_LEFT);
-                $decCientos = substr($decNumberStrFill, 6);
-                $decimales = self::convertGroup($decCientos);
+        $decimales = self::convertDecimales($div_decimales[1] ?? '', $forzarCentimos);
+        $number = str_pad($number, ceil(strlen($number) / 6) * 6, '0', STR_PAD_LEFT);
+        $i = 0;
+        while (strlen($number)) {
+            $strMil = substr($number, -6, 6);
+            $number = substr($number, 0, -6);
+            $n = intval($strMil);
+            if ($n > 0) {
+                if ($i == 0) {
+                    $converted = self::convertMiles($strMil);
+                } else {
+                    $converted = sprintf('%s ', self::convertMiles($strMil) . self::$OTROS[$i] . ($n > 1 ? 'ES' : '')) . $converted;
+                }
             }
-        }
-        else if (count($div_decimales) == 1 && $forzarCentimos){
-            $decimales = 'CERO ';
+            $i++;
         }
 
-        $numberStr = (string) $number;
-        $numberStrFill = str_pad($numberStr, 9, '0', STR_PAD_LEFT);
-        $millones = substr($numberStrFill, 0, 3);
-        $miles = substr($numberStrFill, 3, 3);
-        $cientos = substr($numberStrFill, 6);
-
-        if (intval($millones) > 0) {
-            if ($millones == '001') {
-                $converted .= 'UN MILLON ';
-            } else if (intval($millones) > 0) {
-                $converted .= sprintf('%sMILLONES ', self::convertGroup($millones));
-            }
-        }
-
-        if (intval($miles) > 0) {
-            if ($miles == '001') {
-                $converted .= 'MIL ';
-            } else if (intval($miles) > 0) {
-                $converted .= sprintf('%sMIL ', self::convertGroup($miles));
-            }
-        }
-
-        if (intval($cientos) > 0) {
-            if ($cientos == '001') {
-                $converted .= 'UN ';
-            } else if (intval($cientos) > 0) {
-                $converted .= sprintf('%s ', self::convertGroup($cientos));
-            }
+        if ($converted == '') {
+            $converted = 'CERO ';
         }
 
         if(empty($decimales)){
             $valor_convertido = $converted . strtoupper($moneda);
         } else {
-            $valor_convertido = $converted . strtoupper($moneda) . ' CON ' . $decimales . ' ' . strtoupper($centimos);
+            $valor_convertido = $converted . strtoupper($moneda) . ' CON ' . $decimales . strtoupper($centimos);
         }
 
         return $valor_convertido;
     }
+    private static function convertDecimales($n, $forzarCentimos)
+    {
+        $n = str_pad($n, 2, '0');
+        $n = substr($n, 0, 2);
+        $n = str_pad($n, 3, '0', STR_PAD_LEFT);
+        if (intval($n) == 0 && $forzarCentimos) {
+            return 'CERO ';
+        } else {
+            return self::convertCientos($n);
+        }
+    }
+    private static function convertMiles($n)
+    {
+        $output = '';
+        $miles = substr($n, 0, 3);
+        $cientos = substr($n, 3);
 
-    private static function convertGroup($n)
+        if (intval($miles) > 0) {
+            if ($miles == '001') {
+                $output .= 'MIL ';
+            } else if (intval($miles) > 0) {
+                $output .= sprintf('%sMIL ', self::convertCientos($miles));
+            }
+        }
+
+        if (intval($cientos) > 0) {
+            if ($cientos == '001') {
+                $output .= 'UN ';
+            } else if (intval($cientos) > 0) {
+                $output .= sprintf('%s', self::convertCientos($cientos));
+            }
+        }
+        return $output;
+    }
+    private static function convertCientos($n)
     {
         $output = '';
 
@@ -134,12 +153,12 @@ class NumeroALetras
             $output = self::$CENTENAS[$n[0] - 1];
         }
 
-        $k = intval(substr($n,1));
+        $k = intval(substr($n, 1));
 
         if ($k <= 20) {
             $output .= self::$UNIDADES[$k];
         } else {
-            if(($k > 30) && ($n[2] !== '0')) {
+            if($n[2] !== '0') {
                 $output .= sprintf('%sY %s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
             } else {
                 $output .= sprintf('%s%s', self::$DECENAS[intval($n[1]) - 2], self::$UNIDADES[intval($n[2])]);
